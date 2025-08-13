86da55edf8ec73f50aa119b1a45f012d89b31968
m0rdras <m0rdras@users.noreply.github.com>
2025-08-13 12:38:43 +0200

    increased summarizer word limit to 1000

--- letta/prompts/gpt_summarize.py
+++ letta/prompts/gpt_summarize.py
@@ -1,4 +1,4 @@
-WORD_LIMIT = 100
+WORD_LIMIT = 1000
 SYSTEM = f"""Your job is to summarize a history of previous messages in a conversation between an AI persona and a human.
 The conversation you are given is a from a fixed context window and may not be complete.
 Messages sent by the AI are marked with the 'assistant' role.
